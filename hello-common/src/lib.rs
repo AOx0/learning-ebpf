8757f3aefdd8af3993b2a8569f5047b2a16e51e0
Alejandro Osornio <aoxo.contact@gmail.com>
2024-06-22 11:58:20 -0600

    Changes

--- hello-common/src/lib.rs
+++ hello-common/src/lib.rs
@@ -10,17 +10,19 @@ pub struct Data {
     pub pid: u32,
     pub command: [u8; 16],
     pub message: [u8; 11],
+    pub path: [u8; 64],
 }
 
 impl Debug for Data {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         write!(
             f,
-            "Data {{ uid: {uid}, pid: {pid}, msg: \"{msg}\", cmd: \"{cmd}\" }}",
+            "{uid: <6} {pid: <6} msg: \"{msg}\", cmd: \"{cmd}\", path: \"{path}\"",
             uid = self.uid,
             pid = self.pid,
             msg = self.message.as_bstr(),
             cmd = self.command.as_bstr(),
+            path = self.path.as_bstr()
         )
     }
 }
